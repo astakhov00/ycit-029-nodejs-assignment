3906902af7e2c357223dcd113330729b2aa568f1
= <astakhov00@gmail.com>
2022-12-04 23:19:23 -0500

    Assignment completed

--- server.js
+++ server.js
@@ -26,10 +26,54 @@ app.get("/api/users", (req, res) => {
 });
 
 // Add a new route to get a *SINGLE* user (you can use either path param or query param)
+app.get("/api/users/:id", (req, res) => {
+  const user = data.find((user) => user.id === parseInt(req.params.id));
+  if (!user) {
+    res.status(404).send("The user with the given ID was not found.");
+  }
+  res.json(user);
+});
 // /api/users/1      <-- path param (req.params.id)
-// /api/users?id=1   <-- query param (req.query.id) If you go with query param, just modify the existing endpoint above instead of creating a new endpoint
-
+// /api/users?id=1   <-- query param (req.query.id)
 // BONUS QUESTION - Add routes to implement all the CRUD operations (POST, PUT, DELETE)
+
+// POST - Create a new user
+
+// app.post("/api/users", (req, res) => {
+//   const user = {
+//     id: data.length + 1,
+//     name: req.body.name,
+//     age: req.body.age,
+//   };
+//   data.push(user);
+//   res.json(user);
+// });
+
+// PUT - Update an existing user
+
+// app.put("/api/users/:id", (req, res) => {
+//   const user = data.find((user) => user.id === parseInt(req.params.id));
+//   if (!user) {
+//     res.status(404).send("The user with the given ID was not found.");
+//   }
+//   user.name = req.body.name;
+//   user.age = req.body.age;
+//   res.json(user);
+// });
+
+// DELETE - Delete an existing user
+
+// app.delete("/api/users/:id", (req, res) => {
+//   const user = data.find((user) => user.id === parseInt(req.params.id));
+//   if (!user) {
+//     res.status(404).send("The user with the given ID was not found.");
+//   }
+//   const index = data.indexOf(user);
+//   data.splice(index, 1);
+//   res.json(user);
+// });
+
+
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
